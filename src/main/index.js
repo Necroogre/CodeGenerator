ed1e4d6e0459cd66801991768b2be4d83df92cb0
necroogre <necroogre@163.com>
2017-12-26 15:02:41 +0800

    init

---
+++ src/main/index.js
@@ -0,0 +1,59 @@
+import { app, BrowserWindow, ipcMain } from 'electron'
+var fs = require("fs");
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 563,
+    useContentSize: true,
+    width: 1000,
+    frame: true
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+ipcMain.on('findTmpls', (event, arg) => {
+  fs.readdir(arg, (err, data) => {
+    if (err) throw err;
+    var tmpls = data;
+    event.sender.send('findTmpls-reply', tmpls)
+  });
+})
+
+ipcMain.on('getContent', (event, arg) => {
+  var buffer = fs.readFileSync(arg);
+  event.returnValue = buffer.toString();
+})
---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,92 @@
+<template>
+  <div id="wrapper">
+    <Row>
+      <Input v-model="key" placeholder='JsonObj:{"name":"Hello"}'></Input>
+    </Row>
+    <br>
+    <Row>
+    <Upload
+        multiple
+        type="drag"
+        :before-upload="handleBeforeUpload"
+        action="//jsonplaceholder.typicode.com/posts/">
+        <div style="padding: 20px 0">
+            <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
+            <p>Click here to upload</p>
+        </div>
+    </Upload>        
+    </Row>
+    <Button>导出</Button>
+    <br>
+    <pre>
+      {{code }}
+    </pre>
+  </div>
+</template>
+
+<script>
+import SystemInformation from "./LandingPage/SystemInformation";
+import { ipcRenderer } from "electron";
+import _ from "lodash";
+
+export default {
+  name: "landing-page",
+  data() {
+    return {
+      tmpls: [{ text: "moban1" }, { text: "moban2" }],
+      checkedNames: [],
+      key: '{"name":"Hello"}',
+      code: ""
+    };
+  },
+  created() {},
+  components: { SystemInformation },
+  methods: {
+    open(link) {
+      this.$electron.shell.openExternal(link);
+    },
+    handleBeforeUpload(file) {
+      let vm = this;
+      let path = file.path;
+      let key = vm.key;
+      let replaceJson = "";
+      try {
+        replaceJson = JSON.parse(key);
+      } catch (e) {
+        vm.$Message.error(e.message);
+        return;
+      }
+      let content = ipcRenderer.sendSync("getContent", path);
+      let text = _.template(content)(replaceJson);
+      vm.code = text;
+    }
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro");
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Source Sans Pro", sans-serif;
+}
+pre {
+  border: #ddd 1px solid;
+  padding: 5px;
+}
+#wrapper {
+  padding: 60px 80px;
+}
+
+#logo {
+  height: auto;
+  margin-bottom: 20px;
+  width: 420px;
+}
+</style>
